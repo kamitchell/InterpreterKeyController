bbc3e8275640b9d6117121b0592701ec4eb0c320
Kevin Mitchell <kam@kamit.com>
2008-01-27 14:18:13 -0600

    If we get the plugin loaded, load the PyInterpreter nib.

--- EmbeddedInterpreterAppDelegate.py
+++ EmbeddedInterpreterAppDelegate.py
@@ -10,8 +10,7 @@ from EmbeddedInterpreterPlugIn import *
 from PyObjCTools import NibClassBuilder
 
 class EmbeddedInterpreterAppDelegate(NibClassBuilder.AutoBaseClass):
-
+        
     def applicationDidFinishLaunching_(self, sender):
         if EmbeddedInterpreterPlugIn.alloc().init().plugInLoaded():
-            NSLog(u'Objective-C Plug-In Loaded!')
-            NSBeep()
+            NSBundle.loadNibNamed_owner_("PyInterpreter", self)

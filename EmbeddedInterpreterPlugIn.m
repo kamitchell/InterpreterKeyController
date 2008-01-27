e9f61837cc2ac23d886fbaccf35387704b769f19
Kevin Mitchell <kam@kamit.com>
2008-01-27 14:38:44 -0600

    Start the InterpreterKeyController when the plugin loads.

--- EmbeddedInterpreterPlugIn.m
+++ EmbeddedInterpreterPlugIn.m
@@ -4,13 +4,22 @@
 //
 
 #import "EmbeddedInterpreterPlugIn.h"
-
+#import "InterpreterKeyController.h"
 
 @implementation EmbeddedInterpreterPlugIn
 
 -(BOOL)plugInLoaded
 {
+    [[InterpreterKeyController new] start];
     return YES;
 }
 
 @end
+/*
+Local variables:
+mode: ObjC
+tab-width: 4
+indent-tabs-mode: nil
+c-basic-offset: 4
+End:
+*/

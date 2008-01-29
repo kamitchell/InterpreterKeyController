65f59152f270700d1ae0befa3067b3e670da2864
Kevin Mitchell <kam@kamit.com>
2008-01-28 23:15:34 -0600

    Implement showPythonConsole.
    This function allocates a window controller to manage the PyInterpreter nib.

--- EmbeddedInterpreterAppDelegate.py
+++ EmbeddedInterpreterAppDelegate.py
@@ -6,11 +6,17 @@
 from Foundation import *
 from AppKit import *
 from EmbeddedInterpreterPlugIn import *
+from objc import ivar
 
 from PyObjCTools import NibClassBuilder
 
 class EmbeddedInterpreterAppDelegate(NibClassBuilder.AutoBaseClass):
-        
+    pythonConsoleController = ivar('pythonConsoleController')
+
     def applicationDidFinishLaunching_(self, sender):
-        if EmbeddedInterpreterPlugIn.alloc().init().plugInLoaded():
-            NSBundle.loadNibNamed_owner_("PyInterpreter", self)
+        pass
+
+    def showPythonConsole_(self, sender):
+        if self.pythonConsoleController is None:
+            self.pythonConsoleController = NSWindowController.alloc().initWithWindowNibName_("PyInterpreter")
+        self.pythonConsoleController.showWindow_(self)

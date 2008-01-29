3fb27a80fd8326fd8e5768ada9f5ad69dbb01d01
Kevin Mitchell <kam@kamit.com>
2008-01-29 02:10:23 -0600

    Make this a CurrencyConverter application.
    So that the main window has something to do that we can mess with.

--- EmbeddedInterpreterAppDelegate.py
+++ EmbeddedInterpreterAppDelegate.py
@@ -14,6 +14,25 @@ class EmbeddedInterpreterAppDelegate(NibClassBuilder.AutoBaseClass):
     pythonConsoleController = ivar('pythonConsoleController')
     keyController = ivar('keyController')
 
+    # The input fields have formatters that convert the text
+    # value to a number. If we wouldn't do that, exchangeRate
+    # and dollarsToConvert would be set to strings.
+    #
+    # The alternative is using objc instance variables of the
+    # right type (in this case doubles) and let the Cocoa 
+    # implementation worry about the conversion:
+    #   exchangeRate = ivar('exchangeRate', 'd')
+    #   dollarsToConvert = ivar('dollarsToConvert', 'd')
+	
+    def init(self):
+        self = super(EmbeddedInterpreterAppDelegate, self).init()
+        self.exchangeRate = 3
+        self.dollarsToConvert = 4
+        return self
+    
+    def amountInOtherCurrency(self):
+        return self.dollarsToConvert * self.exchangeRate
+    
     def applicationDidFinishLaunching_(self, sender):
         if EmbeddedInterpreterPlugIn.alloc().init().plugInLoaded():
             from EmbeddedInterpreterPlugIn import InterpreterKeyController
@@ -23,3 +42,8 @@ class EmbeddedInterpreterAppDelegate(NibClassBuilder.AutoBaseClass):
         if self.pythonConsoleController is None:
             self.pythonConsoleController = NSWindowController.alloc().initWithWindowNibName_("PyInterpreter")
         self.pythonConsoleController.showWindow_(self)
+
+EmbeddedInterpreterAppDelegate.setKeys_triggerChangeNotificationsForDependentKey_(
+    [u"dollarsToConvert", u"exchangeRate"],
+    u"amountInOtherCurrency"
+)

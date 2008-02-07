1b5aef7eb6a9019bba321483c82845c9530f2d10
Kevin Mitchell <kam@kamit.com>
2008-02-07 00:45:51 -0600

    Add imports and assignments when starting up Python interpreter.

--- EmbeddedInterpreterAppDelegate.py
+++ EmbeddedInterpreterAppDelegate.py
@@ -10,6 +10,11 @@ from objc import ivar
 
 from PyObjCTools import NibClassBuilder
 
+# Script that will be run in the interpreter when it starts
+startupScript = """from Foundation import *
+from AppKit import *
+appDelegate = NSApplication.sharedApplication().delegate()"""
+
 class EmbeddedInterpreterAppDelegate(NibClassBuilder.AutoBaseClass):
     pythonConsoleController = ivar('pythonConsoleController')
     keyController = ivar('keyController')
@@ -29,18 +34,22 @@ class EmbeddedInterpreterAppDelegate(NibClassBuilder.AutoBaseClass):
         self.exchangeRate = 3
         self.dollarsToConvert = 4
         return self
-    
+
     def amountInOtherCurrency(self):
         return self.dollarsToConvert * self.exchangeRate
     
     def applicationDidFinishLaunching_(self, sender):
         if EmbeddedInterpreterPlugIn.alloc().init().plugInLoaded():
             from EmbeddedInterpreterPlugIn import InterpreterKeyController
-            keyController = InterpreterKeyController.new().start()
+            self.keyController = InterpreterKeyController.new().start()
 
     def showPythonConsole_(self, sender):
         if self.pythonConsoleController is None:
             self.pythonConsoleController = NSWindowController.alloc().initWithWindowNibName_("PyInterpreter")
+            interp = self.pythonConsoleController.window().delegate()
+            for line in startupScript.strip().split('\n'):
+                interp.writeCode_(line + '\n')
+                interp.executeLine_(line)
         self.pythonConsoleController.showWindow_(self)
 
 EmbeddedInterpreterAppDelegate.setKeys_triggerChangeNotificationsForDependentKey_(
